93ceeeb5c7de4d00b5f7318bce9d68dcd2b8fbea
Jan Zavec <janzavec88@gmail.com>
2023-06-06 18:27:19 +0200

    leetcode daily attempt actual solution

--- LeetCodeTasks/LeetCodeDailyConsoleApp/Program.cs
+++ LeetCodeTasks/LeetCodeDailyConsoleApp/Program.cs
@@ -8,8 +8,27 @@ namespace LeetCodeDailyConsoleApp
 {
     internal class Program
     {
+        public static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int commonLength = arr[1] - arr[0]; 
+            for (int i = 1; i < arr.Length - 1; i++) 
+            {
+                if (arr[i + 1] - arr[i] != commonLength)
+                {
+                    return false;
+                }  
+              
+            }
+    
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            int[] arr = { 1, 2, 5 };
+            Console.WriteLine(CanMakeArithmeticProgression(arr));
+            Console.ReadLine();
         }
     }
 }
