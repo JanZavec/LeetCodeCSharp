c0a9f03d8b08bba5226650c8f6d363937831f6c4
Jan Zavec <janzavec88@gmail.com>
2023-06-12 17:47:45 +0200

    leetcode daily

--- LeetCodeTasks/LeetCodeDailyConsoleApp/Program.cs
+++ LeetCodeTasks/LeetCodeDailyConsoleApp/Program.cs
@@ -24,12 +24,44 @@ namespace LeetCodeDailyConsoleApp
             return true;
         }
 
-   
+        public static IList<string> SummaryRanges(int[] nums)
+        {
+            IList<string> result = new List<string>();
+            int counter = 1;
+            if (nums.Length == 1)
+            {
+                result.Add($"{nums[0]}");
+                return result;
+            }
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                if (nums[i] == nums[i - 1] + 1)
+                {
+                    ++counter;
+                    continue;
+                }
+                else
+                {
+
+                    if (nums[i - counter] != nums[i - 1])
+                        result.Add($"{nums[i - counter]}->{nums[i - 1]}");
+                    else
+                        result.Add($"{nums[i - counter]}");
+                    if (i == nums.Length - 1)
+                        result.Add($"{nums[i]}");
+                    counter = 1;
+                }
+
+            }
+            if (counter != 1)
+                result.Add($"{nums[nums.Length - counter]}->{nums[nums.Length - 1]}");
+            return result;
+        }
 
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 5 };
-            Console.WriteLine(CanMakeArithmeticProgression(arr));
+            int[] arr = {0,1,2,4,5,7};
+            Console.WriteLine(SummaryRanges(arr));
             Console.ReadLine();
         }
     }
